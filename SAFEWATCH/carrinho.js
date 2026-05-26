e890fcd3bf542dc36dbfeb8ec398fdb02d1a55a7
Miguel Muniz de Camargo <miguelmunizdecamargo322@gmail.com>
2026-05-26 16:21:03 -0300

    Add files via upload

--- SAFEWATCH/carrinho.js
+++ SAFEWATCH/carrinho.js
@@ -1,47 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Seleciona os elementos da aba lateral e botões de gatilho
+    
     const carrinho = document.getElementById("carrinho-lateral");
     const btnFechar = document.getElementById("fechar-carrinho");
-    const btnAbrir = document.getElementById("abrir-carrinho"); // Novo gatilho do ícone
+    const btnAbrir = document.getElementById("abrir-carrinho"); 
     
-    // Seleciona os botões de mais e menos e o campo de texto da quantidade
+    // Seleciona os botões 
     const btnMais = document.getElementById("btn-mais");
     const btnMenos = document.getElementById("btn-menos");
     const inputQtd = document.getElementById("input-qtd");
     
-    // Seleciona os campos de texto onde os preços e parcelas são exibidos
+    // Seleciona os campos de texto 
     const precoExibido = document.getElementById("preco-exibido");
     const parcelasExibidas = document.getElementById("parcelas-exibidas");
     const subtotalValor = document.getElementById("subtotal-valor");
 
-    // Preço unitário base
-    const PRECO_UNITARIO = 650.00;
+    const PRECO_UNIDADE = 650.00;
 
-    // 1. AÇÃO DE ABRIR A ABA LATERAL AO CLICAR NO ÍCONE DO CARRINHO
-    if (btnAbrir && carrinho) {
+    if (btnAbrir) {
         btnAbrir.addEventListener("click", function () {
             carrinho.classList.add("ativa");
         });
     }
 
-    // 2. AÇÃO DE FECHAR A ABA LATERAL AO CLICAR NO "X"
-    if (btnFechar && carrinho) {
+
+
+    
+    if (btnFechar) {
         btnFechar.addEventListener("click", function () {
             carrinho.classList.remove("ativa");
         });
     }
 
-    // 3. FUNÇÃO QUE CALCULA OS VALORES AUTOMATICAMENTE
-    function atualizarPrecos(quantidade) {
-        const total = PRECO_UNITARIO * quantidade;
-        const valorParcela = total / 12;
 
+
+
+    function atualizarPrecos(quantidade) {
+        const total = PRECO_UNIDADE * quantidade;
+       
+
+        //toLocaleString: Transforma o número em moeda brasileira
         precoExibido.innerText = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
         subtotalValor.innerText = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        //parcelasExibidas.innerText: Atualiza o texto 
         parcelasExibidas.innerText = `ou 12x de ${valorParcela.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
     }
 
-    // 4. EVENTOS DE CLIQUE NOS BOTÕES DE + E -
+
+
+// o & siginifca "e"
+//addEventListener:espera o evento acontecer
     if (btnMais && btnMenos && inputQtd) {
         btnMais.addEventListener("click", function () {
             let qtd = parseInt(inputQtd.value);
